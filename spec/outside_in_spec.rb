4440b9ead0d8ac24660bd57087ee53fa786dcf2c
Dmitry Maksyoma <ledestin@gmail.com>
2020-10-21 00:34:45 +1300

    Spec: extract #convert to run lumione(1) via it

--- spec/outside_in_spec.rb
+++ spec/outside_in_spec.rb
@@ -1,7 +1,14 @@
 describe "lumione(1)" do
   it "converts NZD to USD" do
-    conversion_output = `./bin/lumione 1 nzd usd`
+    conversion_output = convert "1 nzd usd"
 
     expect(conversion_output).to eq "$1.00 NZD ($0.66 USD)\n"
+  end
+
+  private
+
+  def convert(args_string)
+    amount, original_currency, converted_to_currency = args_string.split
+    `./bin/lumione #{amount} #{original_currency} #{converted_to_currency}`
   end
 end
