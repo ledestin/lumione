5c91dcb1e8824f889b45ba6e0716fb066ccf1f7a
Dmitry Maksyoma <ledestin@gmail.com>
2020-10-22 22:42:07 +1300

    Make lumione(1) output messages w/o timestamps in tests

--- spec/outside_in_spec.rb
+++ spec/outside_in_spec.rb
@@ -2,7 +2,7 @@ describe "lumione(1)" do
   it "converts NZD to USD" do
     conversion_output = convert "1 nzd usd"
 
-    expect(conversion_output).to eq "$1.00 NZD ($0.66 USD)\n"
+    expect(conversion_output).to eq "$1.00 NZD ($0.66 USD)"
   end
 
   it "handles invalid amount" do
@@ -16,6 +16,10 @@ describe "lumione(1)" do
 
   def convert(args_string)
     amount, original_currency, converted_to_currency = args_string.split
-    `./bin/lumione #{amount} #{original_currency} #{converted_to_currency}`
+    run_cmd_with_tty "./bin/lumione #{amount} #{original_currency} #{converted_to_currency}"
+  end
+
+  def run_cmd_with_tty(cmd)
+    `socat -ly - EXEC:'#{cmd}',pty,ctty 2>&1`.rstrip
   end
 end
